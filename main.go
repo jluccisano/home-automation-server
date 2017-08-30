048dcb32fe5a8e687215dd80b4bdee1875dd735b
jls <j.luccisano@evs.com>
2017-08-30 14:41:47 +0200

    fix default headers

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ func main() {
 	subConfig := config.GetConf("Prod")
 
 	r := mux.NewRouter()
-
-	r.Handle("/", http.FileServer(http.Dir("./views/")))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.Handle("/", &MyServer{r})
+	//r.Handle("/", http.FileServer(http.Dir("./views/")))
+	//r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 	registerAuth(r, subConfig)
 	registerAlarmControl(r, subConfig)
@@ -22,4 +22,23 @@ func main() {
 
 	http.ListenAndServe(":8514", handlers.LoggingHandler(os.Stdout, r))
 
+}
+
+type MyServer struct {
+	r *mux.Router
+}
+
+func (s *MyServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		rw.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+	// Lets Gorilla work
+	s.r.ServeHTTP(rw, req)
 }
