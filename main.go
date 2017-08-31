21a815a363e9cd562196ce5a07b273962b98aedd
jls <j.luccisano@evs.com>
2017-08-31 15:00:56 +0200

    add server static

--- main.go
+++ main.go
@@ -13,7 +13,11 @@ func main() {
 
 	r := mux.NewRouter()
 
+	// On the default page we will simply serve our static index page.
 	r.Handle("/", http.FileServer(http.Dir("./public/")))
+	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
+	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
+
 
 	registerAuth(r, subConfig)
 	registerAlarmControl(r, subConfig)
